412b8125c3b581688aea9d954691b5dd207030cf
Haining Pan <jack_pan1994@126.com>
2021-08-10 13:30:57 -0400

    Update iter_SDW.m

--- iter_SDW.m
+++ iter_SDW.m
@@ -1,4 +1,5 @@
-params=mainTMD('Nmax',3,'V_t',0,'psi_t',240,'V_b',15,'psi_b',-14,'vz_t',-40,'vz_b',0,'w',0,'nu',[3,3],'n',15,'epsilon',25,'shift',1);
+function iter_SDW(nu)
+params=mainTMD('Nmax',3,'V_t',0,'psi_t',240,'V_b',15,'psi_b',-14,'vz_t',-40,'vz_b',0,'w',0,'nu',nu,'n',15,'epsilon',25,'shift',1);
 [energyall,wfall,valley_index,V1_ave_delta,V2_ave_delta]=energyMF(0,0,0,params);
 [ave1,ave2,occ]=average(energyall,wfall,0,params); 
 
