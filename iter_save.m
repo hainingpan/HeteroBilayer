b7252320fdb4d83d1e4380120068892a8d741eaa
Haining Pan <jack_pan1994@126.com>
2021-08-14 11:02:01 -0700

    Update iter_save.m

--- iter_save.m
+++ iter_save.m
@@ -1,5 +1,5 @@
-function iter_save(nu,Nmax,w,Nk,vz_t,SDW)
-    params=mainTMD('SDW',SDW,'Nmax',Nmax,'V_t',0,'psi_t',240,'V_b',15,'psi_b',-14,'vz_t',vz_t,'vz_b',0,'w',w,'nu',nu,'n',Nk,'epsilon',25,'shift',1);
+function iter_save(nu,Nmax,w,Nk,vz_t,ep)
+    params=mainTMD('SDW',20e-3,'Nmax',Nmax,'V_t',0,'psi_t',240,'V_b',15,'psi_b',-14,'vz_t',vz_t,'vz_b',0,'w',w,'nu',nu,'n',Nk,'epsilon',ep,'shift',1);
     [energyall,wfall,valley_index,V1_ave_delta,V2_ave_delta]=energyMF(0,0,0,params);
     [ave1,ave2,occ]=average(energyall,wfall,0,params); 
     
@@ -23,7 +23,7 @@ function iter_save(nu,Nmax,w,Nk,vz_t,SDW)
             break;
         end
     end
-    fn=sprintf('nu_%d,%d_Nmax%d_w%.1f_Nk_%d_Vzt_%.1f',params.nu(1),params.nu(2),params.Nmax,1000*params.w,Nk,vz_t);
+    fn=sprintf('nu_%d,%d_Nmax%d_w%.1f_Nk_%d_Vzt_%.1f_ep%.1f',params.nu(1),params.nu(2),params.Nmax,1000*params.w,Nk,vz_t,ep);
     [gap,tot,fig_band,fig_spin]=plotline_2(energyall,ave1,ave2,V1_ave_delta,V2_ave_delta,ave1_n,ave2_n,i,strcat('fs',params.chern),params);
     savefig(fig_band,strcat(fn,'_band.fig'))
     savefig(fig_spin,strcat(fn,'_spin.fig'))
