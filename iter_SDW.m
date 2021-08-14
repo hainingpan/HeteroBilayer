2b043a51bec19f84db6f139e321791bb6605a6e6
Haining Pan <jack_pan1994@126.com>
2021-08-13 23:19:20 -0700

    fix fprintf

--- iter_SDW.m
+++ iter_SDW.m
@@ -9,14 +9,14 @@ params=mainTMD('SDW',1e-3,'Nmax',Nmax,'V_t',0,'psi_t',240,'V_b',15,'psi_b',-14,'
 fig_tot=figure;
 gap_list=[gap];
 tot_list=[tot];
-sprintf("%d: Gap=%e (meV), E=%e (meV)",0,gap,tot);
+fprintf("%d: Gap=%e (meV), E=%e (meV)",0,gap,tot);
 for i=1:100
     [energyall,wfall,valley_index,V1_ave_delta,V2_ave_delta]=energyMF(ave1,ave2,i,params);    
     [ave1_n,ave2_n,occ]=average(energyall,wfall,i,params);
     [gap,tot]=plotline_2(energyall,ave1,ave2,V1_ave_delta,V2_ave_delta,ave1_n,ave2_n,i,strcat('fs',params.chern),params);
     gap_list(end+1)=gap;
     tot_list(end+1)=tot;
-    sprintf("%d: Gap=%e (meV), E=%e (meV)",i,gap,tot);
+    fprintf("%d: Gap=%e (meV), E=%e (meV)",i,gap,tot);
     figure(fig_tot);
     plot(tot_list(2:end)*1e3);
     ave1=ave1_n;
