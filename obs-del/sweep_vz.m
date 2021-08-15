1785656682960211c7d40bed50d7ae83dc88f9a7
Haining Pan <jack_pan1994@126.com>
2021-08-15 12:58:27 -0700

    update sweep_vz

---
+++ obs-del/sweep_vz.m
@@ -0,0 +1,60 @@
+function sweep_vz(nu,epsilon)
+    vz_t_list=-38:14;
+    chern_p_list=vz_t_list;
+    chern_m_list=vz_t_list;
+    gap_list=vz_t_list;
+    final_list=vz_t_list;
+    innergap_list=vz_t_list;
+    if nu==[2,1]
+        tsymm=1;
+    else
+        tsymm=0;
+    end
+    for vz_t_index=1:length(vz_t_list)
+        vz_t=vz_t_list(vz_t_index);
+        disp(vz_t);
+        params=mainTMD('Nmax',3,'V_t',0,'psi_t',240,'V_b',15,'psi_b',-14,'vz_t',vz_t,'vz_b',0,'w',20,'nu',nu,'n',15,'epsilon',epsilon,'tsymm',tsymm);
+        [energyall,wfall,valley_index,V1_ave_delta,V2_ave_delta]=energyMF(0,0,params);
+        if nu==[2,1]
+            [ave1,ave2,occ]=average(energyall,wfall,0,1,params); %for QAHE
+        else
+            [ave1,ave2,occ]=average(energyall,wfall,1,0,params); %for QSHE
+        end
+        
+        Nk=size(params.k,1);
+        Nq=size(params.q,1);
+        Nai=size(params.ailist,1);  % The expansion of super cell
+        tot=totalenergy(V1_ave_delta,V2_ave_delta,ave1,ave2,params);
+        tot_list=[tot];
+        
+        for i=1:100
+            tic;
+            [energyall,wfall,valley_index,V1_ave_delta,V2_ave_delta]=energyMF(ave1,ave2,params);    
+            [ave1_n,ave2_n,occ]=average(energyall,wfall,0,1,params);
+            tot=totalenergy(V1_ave_delta,V2_ave_delta,ave1_n,ave2_n,params);
+            tot_list(end+1)=tot;
+            ave1=ave1_n;
+            ave2=ave2_n;
+            if abs(tot_list(end)-tot_list(end-1))<1e-7
+                break;
+            end
+            disp(toc)
+        end
+
+        energyall_sort=sort(energyall(:));
+        gap=energyall_sort(Nk*Nai*params.nu(1)/(params.nu(2))+1)-energyall_sort(Nk*Nai*params.nu(1)/(params.nu(2)));
+        innergap=energyall_sort(Nk*Nai*params.nu(1)/(params.nu(2)))-energyall_sort(Nk*Nai*params.nu(1)/(params.nu(2))-1);
+        finali=i;
+        [chern_p,chern_m]=chern_gs(ave1,ave2,2,params);
+
+        chern_p_list(vz_t_index)=chern_p(1);
+        chern_m_list(vz_t_index)=chern_m(1);
+        gap_list(vz_t_index)=gap;
+        final_list(vz_t_index)=tot;
+        innergap_list(vz_t_index)=innergap;
+        finali_list(vz_t_index)=finali;
+    end
+
+    save(sprintf('phase_nu%d,%d_ep%d.mat',params.nu(1),params.nu(2),epsilon),'chern_p_list','chern_m_list','gap_list','final_list','innergap_list','finali_list','vz_t_list')
+    
+    
--- sweep_vz.m
+++ sweep_vz.m
@@ -1,60 +1,65 @@
-function sweep_vz(nu,epsilon)
-    vz_t_list=-38:14;
+function sweep_vz(nu,Nmax,w,Nk,ep,vz_t_list)
+    % vz_t_list=-50:50;
     chern_p_list=vz_t_list;
     chern_m_list=vz_t_list;
-    gap_list=vz_t_list;
-    final_list=vz_t_list;
-    innergap_list=vz_t_list;
-    if nu==[2,1]
-        tsymm=1;
-    else
-        tsymm=0;
-    end
+    gap_final_list={};
+    tot_final_list={};
+    epoch_list=vz_t_list;
+    ave1=0;
+    ave2=0;
+    epoch=0;
+
     for vz_t_index=1:length(vz_t_list)
         vz_t=vz_t_list(vz_t_index);
-        disp(vz_t);
-        params=mainTMD('Nmax',3,'V_t',0,'psi_t',240,'V_b',15,'psi_b',-14,'vz_t',vz_t,'vz_b',0,'w',20,'nu',nu,'n',15,'epsilon',epsilon,'tsymm',tsymm);
-        [energyall,wfall,valley_index,V1_ave_delta,V2_ave_delta]=energyMF(0,0,params);
-        if nu==[2,1]
-            [ave1,ave2,occ]=average(energyall,wfall,0,1,params); %for QAHE
-        else
-            [ave1,ave2,occ]=average(energyall,wfall,1,0,params); %for QSHE
-        end
-        
-        Nk=size(params.k,1);
-        Nq=size(params.q,1);
-        Nai=size(params.ailist,1);  % The expansion of super cell
-        tot=totalenergy(V1_ave_delta,V2_ave_delta,ave1,ave2,params);
-        tot_list=[tot];
-        
-        for i=1:100
-            tic;
-            [energyall,wfall,valley_index,V1_ave_delta,V2_ave_delta]=energyMF(ave1,ave2,params);    
-            [ave1_n,ave2_n,occ]=average(energyall,wfall,0,1,params);
-            tot=totalenergy(V1_ave_delta,V2_ave_delta,ave1_n,ave2_n,params);
-            tot_list(end+1)=tot;
-            ave1=ave1_n;
-            ave2=ave2_n;
-            if abs(tot_list(end)-tot_list(end-1))<1e-7
-                break;
-            end
-            disp(toc)
-        end
+        fprintf("vz_t=%.1f\n",vz_t);
+        [ave1,ave2,gap_list,tot_list,epoch,chern_p,chern_m]=iter(nu,Nmax,w,Nk,vz_t,ep,ave1,ave2,epoch);
 
-        energyall_sort=sort(energyall(:));
-        gap=energyall_sort(Nk*Nai*params.nu(1)/(params.nu(2))+1)-energyall_sort(Nk*Nai*params.nu(1)/(params.nu(2)));
-        innergap=energyall_sort(Nk*Nai*params.nu(1)/(params.nu(2)))-energyall_sort(Nk*Nai*params.nu(1)/(params.nu(2))-1);
-        finali=i;
-        [chern_p,chern_m]=chern_gs(ave1,ave2,2,params);
-
-        chern_p_list(vz_t_index)=chern_p(1);
-        chern_m_list(vz_t_index)=chern_m(1);
-        gap_list(vz_t_index)=gap;
-        final_list(vz_t_index)=tot;
-        innergap_list(vz_t_index)=innergap;
-        finali_list(vz_t_index)=finali;
+        chern_p_list(vz_t_index)=chern_p;
+        chern_m_list(vz_t_index)=chern_m;
+        gap_final_list{vz_t_index}=gap_list;
+        tot_final_list{vz_t_index}=tot_list;
+        % innergap_list(vz_t_index)=innergap;
+        epoch_list(vz_t_index)=epoch;
     end
 
-    save(sprintf('phase_nu%d,%d_ep%d.mat',params.nu(1),params.nu(2),epsilon),'chern_p_list','chern_m_list','gap_list','final_list','innergap_list','finali_list','vz_t_list')
+    save(sprintf('phase_nu%d,%d_ep%d_w%.1f_Nk_%d.mat',params.nu(1),params.nu(2),epsilon,w,Nk),'chern_p_list','chern_m_list','gap_final_list','tot_final_list','epoch_list','vz_t_list')
+
+
+
+
+function [ave1,ave2,gap_list,tot_list,epoch,chern_p,chern_m]=iter(nu,Nmax,w,Nk,vz_t,ep,ave1,ave2,epoch)
+    params=mainTMD('SDW',20e-3,'Nmax',Nmax,'V_t',0,'psi_t',240,'V_b',15,'psi_b',-14,'vz_t',vz_t,'vz_b',0,'w',w,'nu',nu,'n',Nk,'epsilon',ep,'shift',1);
+    [energyall,wfall,valley_index,V1_ave_delta,V2_ave_delta]=energyMF(ave1,ave2,epoch,params);
+    [ave1_n,ave2_n,occ]=average(energyall,wfall,epoch,params); 
+    
+    [gap,tot]=plotline_2(energyall,ave1,ave2,V1_ave_delta,V2_ave_delta,ave1_n,ave2_n,0,'',params);
+    
+    gap_list=[gap];
+    tot_list=[tot];
+    fprintf("%d: Gap=%.10f (meV), E=%.10f (meV)\n",0,gap*1e3,tot*1e3);
+    ave1=ave1_n;
+    ave2=ave2_n;    
+    epoch=epoch+1;
+    for i=1:500
+        [energyall,wfall,valley_index,V1_ave_delta,V2_ave_delta]=energyMF(ave1,ave2,epoch,params);
+        [ave1_n,ave2_n,occ]=average(energyall,wfall,epoch,params);       
+        [gap,tot]=plotline_2(energyall,ave1,ave2,V1_ave_delta,V2_ave_delta,ave1_n,ave2_n,epoch,'',params);
+        gap_list(end+1)=gap;
+        tot_list(end+1)=tot;
+        ave1=ave1_n;
+        ave2=ave2_n;
+        drawnow;
+        pct_change=((tot_list(end)-tot_list(end-1))/tot_list(end-1));
+        fprintf("%d: Gap=%.10f (meV), E=%.10f (meV), change=%e\n",i,gap*1e3,tot*1e3,pct_change);
+        if abs(pct_change)<1e-7
+            break;
+        end
+        epoch=epoch+1;
+    end
+    fn=sprintf('nu_%d,%d_Nmax%d_w%.1f_Nk_%d_Vzt_%.1f_ep%.1f',params.nu(1),params.nu(2),params.Nmax,1000*params.w,Nk,vz_t,ep);
+    [~,~,fig_band,fig_spin,chern_p,chern_m]=plotline_2(energyall,ave1,ave2,V1_ave_delta,V2_ave_delta,ave1_n,ave2_n,epoch,strcat('fsg',params.chern),params);
+    savefig(fig_band,strcat(fn,'_band.fig'));
+    savefig(fig_spin,strcat(fn,'_spin.fig'));
+    % save(strcat(fn,'.mat'),'gap_list','tot_list','i','chern_p','chern_m');
     
     
